747dcfb8659cd08d32a2b49e035cd75f6bc2fc7e
OOPS_Classroom <udaygangwar111@gmail.com>
2026-03-11 23:46:04 +0530

    vite config updated

--- Velora-Ride-Booking-App-main/frontend/vite.config.js
+++ Velora-Ride-Booking-App-main/frontend/vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    host: '0.0.0.0',   // <== MUST for tunnels to reach your server
-    port: 5173,        // Optional, but recommended
-    strictPort: true   // Fail fast if port is in use
+  
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000
   }
-})
+});
